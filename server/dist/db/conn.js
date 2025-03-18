00868a036735ef88fbea4df38f81ff6fe24d2e8f
Zion Pulapakura <pulapakurazion@gmail.com>
2025-03-18 22:09:18 +1100

    connect to mongodb client

---
+++ server/dist/db/conn.js
@@ -0,0 +1,18 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+let db;
+try {
+    const conn = await client.connect();
+    db = conn.db("admin");
+}
+catch (e) {
+    console.error(e);
+}
+export default db;
