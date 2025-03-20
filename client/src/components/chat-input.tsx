eb4312e5100d6f071b27cf94a541969f5f409fbb
Zion Pulapakura <pulapakurazion@gmail.com>
2025-03-20 22:15:06 +1100

    use jotai for state management of chat input

--- client/src/components/chat-input.tsx
+++ client/src/components/chat-input.tsx
@@ -1,12 +1,15 @@
+import userChatAtom from "@/state/userChatAtom";
 import { IconButton, Group, Input } from "@chakra-ui/react"
 import { IoIosSend } from "react-icons/io";
+import { useAtomValue } from "jotai";
 
 export const ChatInput = () => {
+  const userChat = useAtomValue(userChatAtom)
+
   return (
     <Group rounded='full' paddingX='2' paddingY='1' border="1px solid black">
-      <Input border='none' focusRing='none' placeholder="Ask anything!" >
+      <Input border='none' focusRing='none' placeholder="Ask anything!" value={userChat} />
 
-      </Input>
       <IconButton variant='solid' rounded="full">
         <IoIosSend />
       </IconButton>
---
+++ client/src/state/userChatAtom.ts
@@ -0,0 +1,5 @@
+import { atom } from 'jotai';
+
+const userChatAtom = atom('')
+
+export default userChatAtom
