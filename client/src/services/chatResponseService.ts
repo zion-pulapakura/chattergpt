a91b31ed50126a0f56ce4435921b65757effcfb8
Zion Pulapakura <pulapakurazion@gmail.com>
2025-03-22 00:07:18 +1100

    actually fixed cors problem and got open ai response in client

--- client/src/services/chatResponseService.ts
+++ client/src/services/chatResponseService.ts
@@ -5,17 +5,15 @@ async function chatResponseService(userInput: string) {
     const response = await fetch("http://localhost:3000/chat/respond", {
       method: "POST",
       body: JSON.stringify({ userInput }),
-      headers: corsHeaders()
+      mode: "cors",
+      headers: corsHeaders(),
     });
 
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = await response.json();
-    console.log(json);
-
-
+    const { data } = await response.json();
   } catch (e) {
     console.error((e as Error).message);
   }
--- client/src/utility/cors-headers.ts
+++ client/src/utility/cors-headers.ts
@@ -1,8 +1,9 @@
-function corsHeaders() {
+function corsHeaders(): Headers {
     const headers = new Headers();
 
     headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
     headers.append('Access-Control-Allow-Credentials', 'true');
+    headers.append('Content-Type', 'application/json');
     
     return headers
 }
