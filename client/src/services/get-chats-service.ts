9265dcbe481c04c89b05be5673a571753ab8b0ed
Zion Pulapakura <pulapakurazion@gmail.com>
2025-04-09 09:36:00 +1000

    created service in client to retrieve all chats

---
+++ client/src/services/get-chats-service.ts
@@ -0,0 +1,21 @@
+import corsHeaders from "@/utility/cors-headers";
+
+async function getAllChats() {
+  try {
+    const response = await fetch("http://localhost:3000/chat/all-chats", {
+      mode: "cors",
+      headers: corsHeaders(),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const { userChats, aiChats } = await response.json();
+    return { userChats, aiChats };
+  } catch (e) {
+    console.error(e as Error);
+  }
+}
+
+export default getAllChats;
